6cb9306763ed4029727ca16c75c2ec8ab08d662b
nottak0409 <nottak0409@gmail.com>
2021-11-14 15:32:01 +0900

    drawerの実装

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,28 +1,51 @@
 import React from "react";
 import { createStackNavigator, StackCardInterpolationProps } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { INITIAL, LOADING, HOME, CHOOSE_LOGIN, STATISTICS } from "../../constants/path";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { INITIAL, LOADING, HOME, CHOOSE_LOGIN, STATISTICS, USER_INFO } from "../../constants/path";
 import { Initial, Loading, ChooseLogin } from "../../components/pages";
 import Home from './Home'
 import Statistics from "./Statistics";
 import * as UiContext from '../../contexts/ui'
+import UserInfo from './UserInfo'
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
+const HomeDrawer = createDrawerNavigator()
+const StatisticsDrawer = createDrawerNavigator()
 const forFade = ({ current }: StackCardInterpolationProps ) => ({
   cardStyle: {
     opacity: current.progress,
   },
 })
 
+function HomeWithDrawer() {
+  return (
+    <HomeDrawer.Navigator initialRouteName={HOME}>
+      <HomeDrawer.Screen name={HOME} component={Home} />
+      <HomeDrawer.Screen name={USER_INFO} component={UserInfo}/>
+    </HomeDrawer.Navigator>
+  )
+}
+
+function StatisticsWithDrawer() {
+  return (
+    <StatisticsDrawer.Navigator>
+      <StatisticsDrawer.Screen name={STATISTICS} component={HomeWithDrawer}/>
+      <StatisticsDrawer.Screen name={USER_INFO} component={UserInfo}/>
+    </StatisticsDrawer.Navigator>
+  )
+}
+
 function TabRoutes() {
   return (
     <Tab.Navigator initialRouteName={HOME}>
-      <Tab.Screen name={HOME} component={Home}/>
-      <Tab.Screen name={STATISTICS} component={Statistics}/>
+      <Tab.Screen name={HOME} component={HomeWithDrawer}/>
+      <Tab.Screen name={STATISTICS} component={StatisticsWithDrawer}/>
     </Tab.Navigator>
   )
 }
+
 
 function switchingAuthStatus(status: UiContext.Status) {
   switch(status) {
